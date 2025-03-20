40943d9e6dd63040416e72c93543c2aade08b270
Indulekha A G <95111519+ag-indu@users.noreply.github.com>
2025-03-21 01:55:03 +0530

    Delete Final/Nexlogix directory

--- Final/Nexlogix/space/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-from .views import space_optimization_view
-
-
-urlpatterns = [
-     path('', space_optimization_view, name='space_optimization_view'),  # Default route for space optimization
-]
