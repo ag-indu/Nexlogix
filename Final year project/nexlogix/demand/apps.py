993141084fcd8e3bac57eaeb0a1acc6b94ea8c92
Indulekha A G <95111519+ag-indu@users.noreply.github.com>
2025-03-21 01:54:36 +0530

    Delete Final year project/nexlogix directory

--- Final year project/nexlogix/demand/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DemandConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "demand"
--- Final year project/nexlogix/demand/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index,demand_forecast
-
-urlpatterns = [
-    path("", index, name="index"),
-    path("forecast/", demand_forecast, name="demand_forecast"),
-]
